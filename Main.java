15bd22d38685d0d2073c81326d440bc415dcad11
AreeshaRafique024 <sp23-bcs-024@cuilahore.edu.pk>
2023-10-02 17:29:20 +0500

    first commit

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    Computer computer=new Computer(null, null, 0, 0, false);
+    Employee emp=new Employee(null, null, null);
+    Lab l1=new Lab(null, new Computer(null, null, 0, 0, false) , null, new Employee(null, null, null));
+
+}
